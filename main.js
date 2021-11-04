03bb76ad16fdaac52e0cf6344a256eb45402804c
stefanofilippini <filippini.stefano96@gmail.com>
2021-11-04 16:18:00 +0100

    init + array

---
+++ main.js
@@ -0,0 +1,38 @@
+/* 
+- Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+*/
+
+const cards = [
+    {
+        "nome": 'Wayne Barnett',
+        "ruolo": 'Founder & CEO',
+        "foto": <img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+    }
+    {
+        "nome": 'Angela Caroll',
+        "ruolo": 'Chief Editor',
+        "foto": <img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+    }
+    {
+        "nome": 'Office Manager',
+        "ruolo": 'Founder & CEO',
+        "foto": <img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+    }
+    {
+        "nome": 'Angela Lopez',
+        "ruolo": 'Founder & CEO',
+        "foto": <img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+    }
+    {
+        "nome": 'Scott Estrada',
+        "ruolo": 'Founder & CEO',
+        "foto": <img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+    }
+    {
+        "nome": 'Barbara Ramos',
+        "ruolo": 'Founder & CEO',
+        "foto": <img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett"/>,
+    }
+]
